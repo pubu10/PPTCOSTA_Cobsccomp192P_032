f5507940c1b702484f2df65846445f73967dc7bd
pubudu <tharukapubudu@gmail.com>
2021-11-13 21:29:16 +0530

    Initial Commit

---
+++ PPTCOSTA_Cobsccomp192P_032/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PPTCOSTA_Cobsccomp192P_032
+//
+//  Created by pubudu tharuka on 2021-11-13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PPTCOSTA_Cobsccomp192P_032Tests/PPTCOSTA_Cobsccomp192P_032Tests.swift
@@ -0,0 +1,33 @@
+//
+//  PPTCOSTA_Cobsccomp192P_032Tests.swift
+//  PPTCOSTA_Cobsccomp192P_032Tests
+//
+//  Created by pubudu tharuka on 2021-11-13.
+//
+
+import XCTest
+@testable import PPTCOSTA_Cobsccomp192P_032
+
+class PPTCOSTA_Cobsccomp192P_032Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
